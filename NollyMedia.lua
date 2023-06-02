ab6706da408e01f0d3427a4260871d99af90e124
Lyle Douglass <lyle.douglass@gmail.com>
2023-06-02 00:13:09 -0400

    chore: init commit

---
+++ NollyMedia.lua
@@ -0,0 +1,11 @@
+-- tab size is 4
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cff00fe97CDSoon - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\your_cd_soon.ogg]])
+LSM:Register("sound", "|cff00fe97Incorporeal - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\incorporeal.ogg]])
