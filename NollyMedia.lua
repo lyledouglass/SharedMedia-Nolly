50b4f5aa6bde6352df1d48e1c7666cc959998d60
Lyle Douglass <lyle.douglass@gmail.com>
2023-11-27 00:45:58 -0500

    feat: Everbloom sounds

--- NollyMedia.lua
+++ NollyMedia.lua
@@ -10,3 +10,6 @@ local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU
 LSM:Register("sound", "|cff00fe97CDSoon - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\your_cd_soon.ogg]])
 LSM:Register("sound", "|cff00fe97Incorporeal - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\incorporeal.ogg]])
 LSM:Register("sound", "|cff00fe97Afflicted - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\afflicted.ogg]])
+LSM:Register("sound", "|cff00fe97Glacial Fusion - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\Glacial_Fusion.ogg]])
+LSM:Register("sound", "|cff00fe97Cinderbolt - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\Cinderbolt.ogg]])
+LSM:Register("sound", "|cff00fe97Spatial Compression - Nolly|r", [[Interface\Addons\SharedMedia_Nolly\sound\Spatial_Compression.ogg]])
